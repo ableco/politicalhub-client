4b494242eafee68d6775de376d0794364335199b
Rafael Meza <rhmcord@gmail.com>
2020-11-20 15:34:34 -0500

    Create the Resume component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Layout from "../components/Layout";
+import Resume from "../components/Resume";
 
 export default function Home() {
   return (
     <Layout>
       <h1 className="text-neutral-800">PoliticalHub</h1>
+      <Resume variant="success" />
     </Layout>
   );
 }
