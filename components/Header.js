9b48827ac559d115a113e54f6d471cabf6bfc463
hpneo <hpneo@hotmail.com>
2021-03-28 21:06:19 -0500

    Update Header to show both ubigeo and secondary office

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,12 @@
 import Link from "next/link";
 import titleize from "../utils/titleize";
+import UBIGEOS from "../utils/ubigeos";
 
 export function Header({ candidate, politicalParty }) {
+  const ubigeo = UBIGEOS.find(
+    (ubigeo) => ubigeo.value === candidate?.postulation_ubigeo,
+  );
+
   return (
     <div className="mt-16 flex flex-row justify-center items-center flex-no-wrap">
       <div className="flex flex-col items-stretch justify-start text-center space-y-6">
@@ -25,26 +30,37 @@ export function Header({ candidate, politicalParty }) {
                   `${candidate.names}  ${candidate.family_name} ${candidate.mothers_maiden_name}`,
                 )
               : titleize(politicalParty.name)}
-            {candidate && candidate.postulation_ubigeo !== "000000" ? (
+            {candidate?.office?.match(/congresista/i) ? (
               <span className="inline-block font-semibold text-center leading-11 border-2 rounded text-neutral-700 border-neutral-700 w-10 h-10 ml-4">
                 {candidate.number}
               </span>
             ) : null}
           </h1>
-          {candidate ? (
-            <span className="text-neutral-400 px-5">
-              Candidato/a a {titleize(candidate.office)} por{" "}
-              <Link
-                href={`/political-organizations/${candidate.political_organization.slug}`}
-              >
-                <a className="text-primary-base">
-                  {titleize(candidate.political_organization.name)}
-                </a>
-              </Link>
-            </span>
-          ) : (
-            <span className="text-neutral-400 px-5">Partido Político</span>
-          )}
+          <div className="flex flex-col items-stretch justify-start text-center space-y-1">
+            {candidate ? (
+              <span className="text-neutral-400 px-5">
+                Candidato/a a {titleize(candidate.office)} de{" "}
+                <Link
+                  href={`/political-organizations/${candidate.political_organization.slug}`}
+                >
+                  <a className="text-primary-base">
+                    {titleize(candidate.political_organization.name)}
+                  </a>
+                </Link>
+                {candidate?.office?.match(/congresista/i)
+                  ? ` por ${ubigeo.label}`
+                  : null}
+              </span>
+            ) : (
+              <span className="text-neutral-400 px-5">Partido Político</span>
+            )}
+            {candidate?.secondary_office ? (
+              <span className="text-neutral-400 px-5">
+                Candidato/a a {titleize(candidate?.secondary_office)} por{" "}
+                {ubigeo.label}
+              </span>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
